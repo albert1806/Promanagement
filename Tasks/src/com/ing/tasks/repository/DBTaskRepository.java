71976e7b900835b3e3ca1fefc263ca7f61f4ea72
Studenr <Studenr@PC8>
2015-11-27 11:29:28 +0200

    Implementantion for getTasks for an already existing databse and inputs fron DATABASE

--- Tasks/src/com/ing/tasks/repository/DBTaskRepository.java
+++ Tasks/src/com/ing/tasks/repository/DBTaskRepository.java
@@ -3,6 +3,13 @@
  */
 package com.ing.tasks.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -13,10 +20,12 @@ import java.util.List;
 public class DBTaskRepository implements TaskRepository {
 
 	/**
-	 *  Add task to DB.
+	 * Add task to DB.
 	 *
-	 * @param t the t
-	 * @throws TaskRepositoryException the task repository exception
+	 * @param t
+	 *            the t
+	 * @throws TaskRepositoryException
+	 *             the task repository exception
 	 * @see com.ing.tasks.repository.TaskRepository#addTask(com.ing.tasks.repository.Task)
 	 */
 	@Override
@@ -28,7 +37,8 @@ public class DBTaskRepository implements TaskRepository {
 	/**
 	 * Removes the task.
 	 *
-	 * @param t the t
+	 * @param t
+	 *            the t
 	 * @see com.ing.tasks.repository.TaskRepository#removeTask(com.ing.tasks.repository.Task)
 	 */
 	@Override
@@ -45,16 +55,57 @@ public class DBTaskRepository implements TaskRepository {
 	 */
 	@Override
 	public List<Task> getTheTasks() {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Task> result = new LinkedList<>();
+		Connection conn = null;
+
+		try {
+			// load driver
+			Class.forName("org.hsqldb.jdbcDriver").newInstance();
+			/*
+			 * creaza un obiect, in constructor se inregistreaza driveru
+			 * etc.etc. Multe drivere nu a nevoie de asa ceva ca au metoda
+			 * statica care face asta
+			 */
+			// create connection
+			conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/"/*url*/, "SA"/*username*/,""/*parola*/);
+			// load data in loop
+			String filter = "";
+			PreparedStatement st = conn.prepareStatement("SELECT * FROM TASK WHERE name <> ? ");
+			st.setString(1, filter);
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				boolean completed = rs.getBoolean("completed");
+
+				Task t = new Task(name, description);
+				t.setCompleted(completed);
+
+				result.add(t);
+			}
+
+		} catch (Exception e) { // Nu prinde niciodata in productie cu Exception
+			e.printStackTrace();
+		} finally {
+			// close connection
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 
 	/**
 	 * Read task.
 	 *
-	 * @param name the name
+	 * @param name
+	 *            the name
 	 * @return the task
-	 * @throws TaskRepositoryException the task repository exception
+	 * @throws TaskRepositoryException
+	 *             the task repository exception
 	 * @see com.ing.tasks.repository.TaskRepository#readTask(java.lang.String)
 	 */
 	@Override
@@ -78,7 +129,8 @@ public class DBTaskRepository implements TaskRepository {
 	/**
 	 * Update task.
 	 *
-	 * @param t the t
+	 * @param t
+	 *            the t
 	 * @see com.ing.tasks.repository.TaskRepository#updateTask(com.ing.tasks.repository.Task)
 	 */
 	@Override
